b43778abd8af9470e8394e3c8b592d267f866882
Higor Santos <higoraugusto120@gmail.com>
2020-06-04 11:42:54 -0300

    subindo projeto

---
+++ academico/Controllers/InstituicaoController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace academico.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Hogwarts",
+                Endereco = "Escócia"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Mansão X",
+                Endereco = "Nova York"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        public IActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+        
+            return RedirectToAction("Index");
+        }
+    }
+}
